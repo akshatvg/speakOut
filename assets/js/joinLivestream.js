074ecbac83a3d992d4bc21b7c5c794336e06df9b
Akshat Gupta <akshatvg23@gmail.com>
2020-06-11 00:12:59 +0530

    testing in production

---
+++ assets/js/joinLivestream.js
@@ -0,0 +1,152 @@
+/**
+ * Agora Broadcast Client 
+ */
+
+var agoraAppId = 'a6af85f840ef43108491705e2315a857'; // set app id
+var channelName = 'AgoraBroadcastDemo'; // set channel name
+
+// create client 
+var client = AgoraRTC.createClient({mode: 'live', codec: 'vp8'}); // vp8 to work across mobile devices
+
+// set log level:
+// -- .DEBUG for dev 
+// -- .NONE for prod
+AgoraRTC.Logger.setLogLevel(AgoraRTC.Logger.DEBUG); 
+$( document ).ready( function() {
+  // Due to broswer restrictions on auto-playing video, 
+  // user must click to init and join channel
+  $("#watch-live-btn").click(function(){
+    console.log("user clicked to watch broadcast")
+    // init Agora SDK
+    client.init(agoraAppId, function () {
+      $("#watch-live-overlay").remove();
+      console.log('AgoraRTC client initialized');
+      joinChannel(); // join channel upon successfull init
+    }, function (err) {
+      console.log('[ERROR] : AgoraRTC client init failed', err);
+    });
+  });
+});
+
+client.on('stream-published', function (evt) {
+  console.log('Publish local stream successfully');
+});
+
+// connect remote streams
+client.on('stream-added', function (evt) {
+  var stream = evt.stream;
+  var streamId = stream.getId();
+  console.log('New stream added: ' + streamId);
+  console.log('Subscribing to remote stream:' + streamId);
+  // Subscribe to the stream.
+  client.subscribe(stream, function (err) {
+    console.log('[ERROR] : subscribe stream failed', err);
+  });
+});
+
+client.on('stream-removed', function (evt) {
+  var stream = evt.stream;
+  stream.stop(); // stop the stream
+  stream.close(); // clean up and close the camera stream
+  console.log("Remote stream is removed " + stream.getId());
+});
+
+client.on('stream-subscribed', function (evt) {
+  var remoteStream = evt.stream;
+  var remoteId =  remoteStream.getId();
+  // remoteStream.play('full-screen-video');
+  console.log('Successfully subscribed to remote stream: ' + remoteId);
+  if( $('#full-screen-video').is(':empty') ) { 
+    mainStreamId = remoteId;
+    remoteStream.play('full-screen-video');
+  } else {
+    addRemoteStreamMiniView(remoteStream);
+  }
+});
+
+// remove the remote-container when a user leaves the channel
+client.on('peer-leave', function(evt) {
+  console.log('Remote stream has left the channel: ' + evt.uid);
+  evt.stream.stop(); // stop the stream
+});
+
+// show mute icon whenever a remote has muted their mic
+client.on('mute-audio', function (evt) {
+  var remoteId = evt.uid;
+});
+
+client.on('unmute-audio', function (evt) {
+  var remoteId = evt.uid;
+});
+
+// show user icon whenever a remote has disabled their video
+client.on('mute-video', function (evt) {
+  var remoteId = evt.uid;
+});
+
+client.on('unmute-video', function (evt) {
+  var remoteId = evt.uid;
+});
+
+// ingested live stream 
+client.on('streamInjectedStatus', function (evt) {
+  console.log("Injected Steram Status Updated");
+  // evt.stream.play('full-screen-video');
+  console.log(JSON.stringify(evt));
+}); 
+
+// join a channel
+function joinChannel() {
+  var token = generateToken();
+
+  // set the role
+  client.setClientRole('audience', function() {
+    console.log('Client role set to audience');
+  }, function(e) {
+    console.log('setClientRole failed', e);
+  });
+  
+  client.join(token, channelName, 0, function(uid) {
+      console.log('User ' + uid + ' join channel successfully');
+  }, function(err) {
+      console.log('[ERROR] : join channel failed', err);
+  });
+}
+
+function leaveChannel() {
+  client.leave(function() {
+    console.log('client leaves channel');
+  }, function(err) {
+    console.log('client leave failed ', err); //error handling
+  });
+}
+
+// use tokens for added security
+function generateToken() {
+  return null; // TODO: add a token generation
+}
+
+
+// REMOTE STREAMS UI
+function addRemoteStreamMiniView(remoteStream){
+  var streamId = remoteStream.getId();
+  // append the remote stream template to #remote-streams
+  $('#external-broadcasts-container').append(
+    $('<div/>', {'id': streamId + '_container',  'class': 'remote-stream-container col'}).append(
+      $('<div/>', {'id': streamId + '_mute', 'class': 'mute-overlay'}).append(
+          $('<i/>', {'class': 'fas fa-microphone-slash'})
+      ),
+      $('<div/>', {'id': streamId + '_no-video', 'class': 'no-video-overlay text-center'}).append(
+        $('<i/>', {'class': 'fas fa-user'})
+      ),
+      $('<div/>', {'id': 'agora_remote_' + streamId, 'class': 'remote-video'})
+    )
+  );
+  remoteStream.play('agora_remote_' + streamId); 
+
+  var containerId = '#' + streamId + '_container';
+  $(containerId).dblclick(function() {
+    client.removeInjectStreamUrl(injectedStreamURL);
+    $(containerId).remove();
+  });
+}
---
+++ assets/js/ui.js
@@ -0,0 +1,92 @@
+
+// UI buttons
+function enableUiControls() {
+
+  $("#mic-btn").prop("disabled", false);
+  $("#video-btn").prop("disabled", false);
+  $("#exit-btn").prop("disabled", false);
+
+  $("#mic-btn").click(function () {
+    toggleMic();
+  });
+
+  $("#video-btn").click(function () {
+    toggleVideo();
+  });
+
+  $("#exit-btn").click(function () {
+    console.log("so sad to see you leave the channel");
+    leaveChannel();
+  });
+
+  // $("#start-RTMP-broadcast").click(function () {
+  //   startLiveTranscoding();
+  //   $('#addRtmpConfigModal').modal('toggle');
+  //   $('#rtmp-url').val('');
+  // });
+
+  // $("#add-external-stream").click(function () {
+  //   addExternalSource();
+  //   $('#add-external-source-modal').modal('toggle');
+  // });
+
+  // keyboard listeners 
+  $(document).keypress(function (e) {
+
+    switch (e.key) {
+      case "m":
+        console.log("squick toggle the mic");
+        toggleMic();
+        break;
+      case "v":
+        console.log("quick toggle the video");
+        toggleVideo();
+        break;
+      case "q":
+        console.log("so sad to see you quit the channel");
+        leaveChannel();
+        break;
+      default:  // do nothing
+    }
+  });
+}
+
+function toggleBtn(btn) {
+  btn.toggleClass('btn-dark').toggleClass('btn-danger');
+}
+
+function toggleVisibility(elementID, visible) {
+  if (visible) {
+    $(elementID).attr("style", "display:block");
+  } else {
+    $(elementID).attr("style", "display:none");
+  }
+}
+
+function toggleMic() {
+  toggleBtn($("#mic-btn")); // toggle button colors
+  toggleBtn($("#mic-dropdown"));
+  $("#mic-icon").toggleClass('fa-microphone').toggleClass('fa-microphone-slash'); // toggle the mic icon
+  if ($("#mic-icon").hasClass('fa-microphone')) {
+    localStreams.camera.stream.unmuteAudio(); // enable the local mic
+  } else {
+    localStreams.camera.stream.muteAudio(); // mute the local mic
+  }
+}
+
+function toggleVideo() {
+  toggleBtn($("#video-btn")); // toggle button colors
+  toggleBtn($("#cam-dropdown"));
+  if ($("#video-icon").hasClass('fa-video')) {
+    localStreams.camera.stream.muteVideo(); // enable the local video
+    console.log("muteVideo");
+  } else {
+    localStreams.camera.stream.unmuteVideo(); // disable the local video
+    console.log("unMuteVideo");
+  }
+  $("#video-icon").toggleClass('fa-video').toggleClass('fa-video-slash'); // toggle the video icon
+}
+
+$("#mic-btn").prop("disabled", true);
+$("#video-btn").prop("disabled", true);
+$("#exit-btn").prop("disabled", true);
